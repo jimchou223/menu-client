149a627ce2a0b0aef4523b75bc730e01018c5968
jimchou223 <ethan790223@gmail.com>
2019-12-06 16:03:03 -0800

    add filter function

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-const filterNewRow = document.getElementById('filter-new-row')
-const filterDeleteRow = document.getElementById('filter-remove-row')
+const dangerFilterNewRow = document.getElementById('danger-filter-new-row')
+const dangerFilterDeleteRow = document.getElementById('danger-filter-remove-row')
+const dangerFilterInput = document.getElementById('danger-filter-input')
+
+const warningFilterNewRow = document.getElementById('warning-filter-new-row')
+const warningFilterDeleteRow = document.getElementById('warning-filter-remove-row')
+const warningFilterInput = document.getElementById('warning-filter-input')
+
 const filterInput = document.getElementById('filter-input')
 
 const clear = document.getElementById('clear')
 
-const filterNode = document.getElementById('filter-node')
+const dangerFilterNode = document.getElementById('danger-filter-node')
+const warningFilterNode = document.getElementById('warning-filter-node')
+
 const resultArea = document.getElementById('result-area')
 
-filterNewRow.addEventListener('click', function (e) {
+dangerFilterNewRow.addEventListener('click', function (e) {
     e.preventDefault()
     let newInput = document.createElement('input')
-    newInput.setAttribute('class', 'filter-input form-control')
+    newInput.setAttribute('class', 'danger-filter-input form-control')
     newInput.required = true
-    if (filterNode.childNodes.length <= 13) {
-        filterNode.appendChild(newInput)
+    if (dangerFilterNode.childNodes.length <= 13) {
+        dangerFilterNode.appendChild(newInput)
     }
 
 })
 
-filterDeleteRow.addEventListener('click', function (e) {
+dangerFilterDeleteRow.addEventListener('click', function (e) {
     e.preventDefault()
-    if (filterNode.childNodes.length > 3) {
-        filterNode.removeChild(filterNode.lastChild)
+    if (dangerFilterNode.childNodes.length > 3) {
+        dangerFilterNode.removeChild(dangerFilterNode.lastChild)
+    }
+})
+
+warningFilterNewRow.addEventListener('click', function (e) {
+    e.preventDefault()
+    let newInput = document.createElement('input')
+    newInput.setAttribute('class', 'warning-filter-input form-control')
+    newInput.required = true
+    if (warningFilterNode.childNodes.length <= 13) {
+        warningFilterNode.appendChild(newInput)
+    }
+
+})
+
+warningFilterDeleteRow.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (warningFilterNode.childNodes.length > 3) {
+        warningFilterNode.removeChild(warningFilterNode.lastChild)
     }
 })
 
@@ -63,20 +89,35 @@ function categorize(arr, data) {
 }
 
 
-function checkIngredient(arr, filterArr) {
+function checkIngredientDanger(arr, filterArr) {
     arr.forEach((el) => {
         for (let i = 0; i < el.ingredients.length; i++) {
-            for (let j = 0; j < filterArr.length; j++) {
-                if (el.ingredients[i].includes(filterArr[j])) {
-                    el.ok = false;
+            for (let j = 0; j < filterArr.danger.length; j++) {
+                if (el.ingredients[i].includes(filterArr.danger[j])) {
+                    el.danger = true;
                     return
-                } else {
-                    el.ok = true
                 }
             }
 
         }
     })
+    console.log(arr)
+    return arr
+}
+
+function checkIngredientWarning(arr, filterArr) {
+    arr.forEach((el) => {
+        for (let i = 0; i < el.ingredients.length; i++) {
+            for (let j = 0; j < filterArr.warning.length; j++) {
+                if (el.ingredients[i].includes(filterArr.warning[j])) {
+                    el.warning = true;
+                    return
+                }
+            }
+
+        }
+    })
+    console.log(arr)
     return arr
 }
 
@@ -135,10 +176,12 @@ function showResult(arr, chosenSet, uniqueArr) {
 
     arr.forEach((el) => {
         targetArea = document.querySelector(`.result-${el.set}`)
-        if (el.ok) {
-            targetArea.innerHTML += `<div><div><p class="card-text dishname-no-i">${el.category}. ${el.name}</p></div> <p class="detail">${el.ingredients}</p></div>`
+        if (el.danger) {
+            targetArea.innerHTML += `<div><div style="display: flex;"><i class="fas fa-skull-crossbones icon-danger"></i><p class="card-text dishname">${el.category}. ${el.name}</p></div> <p class="detail">${el.ingredients}<p></div>`
+        } else if (el.warning) {
+            targetArea.innerHTML += `<div><div style="display: flex;"><i class="fas fa-exclamation-triangle icon-warning"></i><p class="card-text dishname">${el.category}. ${el.name}</p></div> <p class="detail">${el.ingredients}<p></div>`
         } else {
-            targetArea.innerHTML += `<div><div style="display: flex;"><i class="fas fa-skull-crossbones icon"></i><p class="card-text dishname">${el.index}. ${el.name}</p></div> <p class="detail">${el.ingredients}<p></div>`
+            targetArea.innerHTML += `<div><div><p class="card-text dishname-no-i">${el.category}. ${el.name}</p></div> <p class="detail">${el.ingredients}</p></div>`
         }
     })
 }
@@ -146,21 +189,35 @@ function showResult(arr, chosenSet, uniqueArr) {
 
 
 filterInput.addEventListener('submit', (e) => {
-    console.log('start')
-    const filtersInput = document.querySelectorAll('.filter-input')
+    e.preventDefault()
+    console.log('click')
+    const dangerFiltersInput = document.querySelectorAll('.danger-filter-input')
+
+    const warningFiltersInput = document.querySelectorAll('.warning-filter-input')
+    console.log(warningFiltersInput)
     const chosenSet = document.getElementById('chosen-set').value
     let filters = {
         set: '',
-        filters: []
+        danger: [],
+        warning: []
+
     }
-    filtersInput.forEach(el => {
+    filters.set = chosenSet
+    dangerFiltersInput.forEach(el => {
         if (el.value != '') {
-            filters.filters.push(el.value)
+            filters.danger.push(el.value)
         }
 
     })
-    if (filters.filters.length !== 0) {
-        e.preventDefault()
+    warningFiltersInput.forEach(el => {
+        if (el.value != '') {
+            filters.warning.push(el.value)
+        }
+
+    })
+    console.log(filters)
+    if (filters.danger.length !== 0) {
+        // e.preventDefault()
         $.ajax({
             // url: 'http://localhost:3000/search',
             url: 'https://menu-server-jim.herokuapp.com/search',
@@ -171,10 +228,13 @@ filterInput.addEventListener('submit', (e) => {
             success: async function (data, textStatus, jqXHR) {
                 let uniqueSetList = findUnique(data)
                 let uniqueArr = createUniqueArr(uniqueSetList)
-                checkIngredient(data, filters.filters)
+                console.log(data)
+                checkIngredientDanger(data, filters)
+                checkIngredientWarning(data, filters)
                 countRank(data, uniqueArr)
                 sortArr(uniqueArr)
                 showResult(data, chosenSet, uniqueArr)
+                
 
             }
         })
